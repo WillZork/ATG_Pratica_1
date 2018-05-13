14d6aaf6c4907e59a79a21b5d14253573d0654b7
Marcos Junior <marcos.junio@ccc.ufcg.edu.br>
2018-05-13 18:26:21 -0300

    adição da classe testes de grafos com peso

---
+++ ATG_P1/src/tests/WeightedGraphTests.java
@@ -0,0 +1,28 @@
+/**
+ * UFCG - Universidade Federal de Campina Grande
+ * Aplicacao de Teoria dos Grafos
+ * - Luan Carlos da Silva Bezerra
+ * -
+ * -
+ * -
+ * -
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WeightedGraphTests {
+
+	@Before
+	public void setUp() {
+		
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
